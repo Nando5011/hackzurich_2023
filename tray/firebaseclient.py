da77b0f885353bf991b2a8690da821bcd8ea55dc
Florian Thiévent <florianjeremias@gmail.com>
2023-09-16 10:23:09 +0200

    feat: added more code for tray module

--- tray/firebaseclient.py
+++ tray/firebaseclient.py
@@ -1,15 +1,44 @@
 from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials, db
+from dotenv import load_dotenv
+import os 
+
+"""
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+"""
 
 
 class FirebaseClient:
 
+
     def __init__(self) -> None:
-        self.firebase_project = "hackzurich23-8212"
-        self.firebase  = firebase.FirebaseApplication('https://firestore.googleapis.com/v1/projects/{}/databases/'.format(self.firebase_project), None)
+        
+        load_dotenv()
+
+        self.firebase_credentials = credentials.Certificate({
+            "apiKey": "AIzaSyCzUSeLToQ1X1vNW1brnkcHcWJif33HRxE",
+            "authDomain": "hackzurich23-8212.firebaseapp.com",
+            "projectId": "hackzurich23-8212",
+            "storageBucket": "hackzurich23-8212.appspot.com",
+            "messagingSenderId": "432778287112",
+            "appId": "1:432778287112:web:57017c368ff6c03d4899bd",
+        })
+        
+        self.firebase = firebase_admin.initialize_app(self.firebase_credentials)
+        self.ref = db.reference('/users')
+        print(self.ref)
     
     def authenticate(self)->None:
         pass
     
     def get_users(self) -> None:
-        result = self.firebase.get('https://firestore.googleapis.com/v1/projects/{}/firestore/users'.format(self.firebase_project), None)
-        print(result)
+        pass
+
+
+
+
--- tray/start_tray.py
+++ tray/start_tray.py
@@ -5,30 +5,64 @@ import qrcode
 import uuid
 from PIL import Image
 from firebaseclient import FirebaseClient
+from screeninfo import get_monitors
+import sys
+
+
+class QRApi:
+    def window_blur(self):
+        print("blurred")
 
 
 class Tray:
 
     def __init__(self) -> None:
+        
         self.menu = (
             pystray.MenuItem('QR Code', self.on_open_qrcode_window),
             pystray.MenuItem('Exit', self.on_exit),
         )
 
-        self.firebase_client = FirebaseClient()
-        self.firebase_client.get_users()
+        #self.firebase_client = FirebaseClient()
+
 
         self.image = Image.open(os.path.join(os.getcwd(), 'tray', 'logitech.png'))  # Replace with the path to your icon image
         self.icon = pystray.Icon("MyApp", self.image, menu=self.menu)
 
-        self.qrcode_window = webview.create_window('Woah dude!', html='<h1>Woah dude! Take a Break<h1>', on_top=True, resizable=False, frameless=True)
+        self.qrcode_window = webview.create_window(
+            'QRCode', 
+            'qrcode.html', 
+            on_top=True, 
+            resizable=False, 
+            frameless=True, 
+            width=250, 
+            height=320, 
+            y=0, 
+            x=self.calculate_position_of_tray_view(250, 320),
+            js_api=QRApi()
+            )
         self.qrcode_window.events.closing += self.on_closing
         self.qrcode_window_is_open = False
 
         self.uuid = self.generate_uuid()
         self.generate_qrcode(self.uuid)
     
-    def init_firebase() -> None:
+    def calculate_position_of_tray_view(self, dimensions:(int,int) = (0,0)) -> (int, int):
+        for m in get_monitors():
+            if m.is_primary:
+                    
+                if sys.platform == 'darwin':
+                    return (round(m.width*0.6), 0)
+                if sys.platform == 'win32' or sys.platform == 'cygwin' or sys.platform == 'msys':
+                    return (round(m.width * 0.6), round(m.height-dimensions[1]))
+                
+       
+                    
+                
+                
+
+    
+    def init_firebase(self) -> None:    
         pass
 
     def generate_uuid(self) -> str:
@@ -42,7 +76,7 @@ class Tray:
 
     def on_open_qrcode_window(self):
         if not self.qrcode_window_is_open:
-            webview.start(self.qrcode_window)
+            webview.start(self.qrcode_window, debug=True)
             self.qrcode_window_is_open = True
 
 
@@ -58,7 +92,6 @@ class Tray:
 
 
 if __name__ == '__main__':
-
     tray_app = Tray()
     tray_app.run()
 
