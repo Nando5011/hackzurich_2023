9ee2825c49812402dc7359b229c19972f193d415
Nando5011 <Nando.imboden@bluewin.ch>
2023-09-16 11:10:08 +0200

    Finished first version of KeyLogger

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,42 +1,44 @@
-from pynput.keyboard import Key, Listener
 from pynput import keyboard
-import logging
 import datetime
 from datetime import timedelta
 import time
 import os
 
-MAX_SHORTIMEHISTORYTIMESPAN = timedelta(seconds=300)
-
-def getTimeStampFromHistoryEntry(entry):
-    return entry[1]
-
+# Sets up a keyboard listener and logs the keyboard inputs.
+# A history of the last key presses can be accesses via getShortTimeHistory()
+# How long this history is is set via the maxShortTimeHistoryTimeSpan parameter
 class KeyLogger:
-    def __init__(self):
+    def __init__(self, maxShortTimeHistoryTimeSpan: datetime):
         self.shortTimeHistory = []
-        self.longTimeHistory = []
-
+        listener = keyboard.Listener(on_press=self.__onKeyPress)
+        listener.start()
+        self.MAX_SHORTIMEHISTORYTIMESPAN = maxShortTimeHistoryTimeSpan
     # Get a timeDelta from the given comparetime (as string) and the current system time
-    def getTimeDeltaToNow(self, compareTime):
+    def __getTimeDeltaToNow(self, compareTime):
         currentTime = datetime.datetime.now()
         timeDelta = currentTime - compareTime
         return timeDelta
 
+    def __getTimeStampFromHistoryEntry(self, entry):
+        return entry[1]
+
     # Callback function that has to be called on keypress
-    def onKeyPress(self, keyStr):
-        self.shortTimeHistory.append((keyStr, datetime.datetime.now()))
+    def __onKeyPress(self, key):
         try:
+            keyStr = str(key)
+            # Add keypress to short time history
+            self.shortTimeHistory.append((keyStr, datetime.datetime.now()))
             if(len(self.shortTimeHistory) != 0):
-                earliestTimeStampInHistory = getTimeStampFromHistoryEntry(self.shortTimeHistory[0])
-                shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(earliestTimeStampInHistory)
+                earliestTimeStampInHistory = self.__getTimeStampFromHistoryEntry(self.shortTimeHistory[0])
+                shortTimeHistoryTimeSpan = self.__getTimeDeltaToNow(earliestTimeStampInHistory)
             else:
                 shortTimeHistoryTimeSpan = timedelta(hours=0, minutes=0, seconds=0)
             # Remove elements as long as the time span in the shortTimeHistory is >5min
-            while(shortTimeHistoryTimeSpan.seconds > MAX_SHORTIMEHISTORYTIMESPAN.seconds):
+            while(shortTimeHistoryTimeSpan.seconds > self.MAX_SHORTIMEHISTORYTIMESPAN.seconds):
                 self.shortTimeHistory.pop(0)
                 if(len(self.shortTimeHistory) != 0):
-                    earliestTimeStampInHistory = getTimeStampFromHistoryEntry(self.shortTimeHistory[0])
-                    shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(earliestTimeStampInHistory)
+                    earliestTimeStampInHistory = self.__getTimeStampFromHistoryEntry(self.shortTimeHistory[0])
+                    shortTimeHistoryTimeSpan = self.__getTimeDeltaToNow(earliestTimeStampInHistory)
                 else:
                     shortTimeHistoryTimeSpan = timedelta(hours=0, minutes=0, seconds=0)
         except Exception as inst:
@@ -45,20 +47,3 @@ class KeyLogger:
     # Get short time history. The STH shows every keystroke in the configured time span
     def getShortTimeHistory(self):
         return self.shortTimeHistory
-
-mykeyLogger = KeyLogger()
-
-def on_press(key):
-    mykeyLogger.onKeyPress(str(key))
-
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
-while(1):
-    print("STH:")
-    shortTimeHistory = mykeyLogger.getShortTimeHistory()
-    for item in shortTimeHistory:
-        print(item)
-    time.sleep(0.5)
-    # Clearing the Screen
-    os.system('cls')
