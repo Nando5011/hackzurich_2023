2b7c405d13e4ba48f43a16ec3aee2cf8e1277d39
Nando5011 <Nando.imboden@bluewin.ch>
2023-09-16 05:12:14 +0200

    Fixed a bit more, still broken

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -13,18 +13,24 @@ class KeyLogger:
 
     def getTimeDeltaToNow(self, compareTime):
         currentTime = datetime.datetime.now()
-        timeDelta = currentTime - compareTime
+        timeDelta = currentTime - datetime.datetime.strptime(compareTime, '%H:%M:%S')
         return timeDelta
 
     def onKeyPress(self, keyStr):
         self.shortTimeHistory.append(keyStr + datetime.datetime.now().strftime('%H:%M:%S'))
         # TODO fix this: parse the str
-        shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(self.shortTimeHistory[0])
+        if(self.shortTimeHistory):
+            shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(self.shortTimeHistory[0][len(keyStr):])
+        else:
+            shortTimeHistoryTimeSpan = timedelta(hours=0, minutes=0, seconds=0)
         MAX_SHORTIMEHISTORYTIMESPAN = timedelta(minutes=5)
         # Remove elements as long as the time span in the shortTimeHistory is >5min
         while(shortTimeHistoryTimeSpan > MAX_SHORTIMEHISTORYTIMESPAN):
             self.shortTimeHistory.pop(0)
-            shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(self.shortTimeHistory[0])
+            if(self.shortTimeHistory):
+                shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(self.shortTimeHistory[0][len(keyStr):])
+            else:
+                shortTimeHistoryTimeSpan = timedelta(hours=0, minutes=0, seconds=0)
 
     def getShortTimeHistory(self):
         return self.shortTimeHistory
