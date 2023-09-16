4d1ea557692f7279d945af73829b80c14f072c33
Jerry <agboojer@students.zhaw.ch>
2023-09-16 22:44:41 +0200

    fix firebase fetching

--- frontend/src/components/statistic-view/statistic-view.tsx
+++ frontend/src/components/statistic-view/statistic-view.tsx
@@ -1,22 +1,10 @@
-import {
-  Block,
-  BlockTitle,
-  Icon,
-  List,
-  ListInput,
-  Page,
-  View,
-} from "framework7-react";
-import React, { useCallback, useEffect, useState } from "react";
-import {
-  useCollection,
-  useDocument,
-  useDocumentData,
-} from "react-firebase-hooks/firestore";
+import { Block, BlockTitle, Icon, List, ListInput } from "framework7-react";
+import React, { useEffect, useState } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 import { User } from "firebase/auth";
 import { firestore } from "../../js/firebase";
-import { collection, getDocs, onSnapshot, query } from "firebase/firestore";
+import { collection, getDocs, query } from "firebase/firestore";
 import { StatRecord, months } from "../../js/types";
 import YearTimeline from "../timeline/year";
 import store from "../../js/store";
@@ -29,16 +17,62 @@ const StatisticView = ({ currentUser }) => {
   );
 
   const [value, loading, error] = useCollection(
-    collection(firestore, devicesRef),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    }
+    collection(firestore, devicesRef)
   );
 
   useEffect(() => {
+    fetchData();
+  }, [selectedDevice]);
+
+  const fetchData = () => {
     const matrix = {};
-    setStatMatrix({});
-    const unsubscribe = onSnapshot(
+    const qRecords = query(
+      collection(firestore, devicesRef + "/" + selectedDevice + "/records")
+    );
+    getDocs(qRecords).then((qS) => {
+      qS.forEach((doc) => {
+        const qTimestamps = query(
+          collection(
+            firestore,
+            devicesRef +
+              "/" +
+              selectedDevice +
+              "/records/" +
+              doc.id +
+              "/timestamps"
+          )
+        );
+        getDocs(qTimestamps)
+          .then((qS2) => {
+            const dateSplit = doc.id.split("-");
+            qS2.docs.map((doc3) => {
+              if (!matrix[dateSplit[0]]) {
+                matrix[dateSplit[0] + ""] = [];
+              }
+              if (!matrix[dateSplit[0]][months[dateSplit[1]]]) {
+                matrix[dateSplit[0] + ""][months[dateSplit[1] + ""]] = [];
+              }
+              if (
+                !matrix[dateSplit[0]][months[dateSplit[1]]][dateSplit[2] + ""]
+              ) {
+                matrix[dateSplit[0] + ""][months[dateSplit[1] + ""]][
+                  dateSplit[2] + ""
+                ] = [];
+              }
+
+              // Push doc3.data() into the array
+              matrix[dateSplit[0] + ""][months[dateSplit[1] + ""]][
+                dateSplit[2] + ""
+              ].push({ ...doc3.data(), timestamp: doc3.id });
+            });
+          })
+          .then(() => {
+            store.dispatch("setStatsMatrix", matrix);
+            setStatMatrix({ ...(matrix as StatRecord) });
+          });
+      });
+    });
+    /*return onSnapshot(
       collection(firestore, devicesRef + "/" + selectedDevice + "/records"),
       (querySnapshot) => {
         querySnapshot.forEach((doc) => {
@@ -80,10 +114,12 @@ const StatisticView = ({ currentUser }) => {
           );
         });
       }
-    );
-    return () => {
-      unsubscribe();
-    };
+    );*/
+  };
+
+  useEffect(() => {
+    setStatMatrix({});
+    fetchData();
   }, [selectedDevice]);
   return (
     <Block>
@@ -99,11 +135,7 @@ const StatisticView = ({ currentUser }) => {
           <option value={"-"}></option>
           {value &&
             value.docs.map((doc) => (
-              <option
-                key={doc.id}
-                value={doc.id}
-                onSelect={(event) => console.log(event)}
-              >
+              <option key={doc.id} value={doc.id}>
                 {doc.id}
               </option>
             ))}
