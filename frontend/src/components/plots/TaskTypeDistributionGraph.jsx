500b18f1ad9a4a8fade7959e77c605789f278319
Hamani Mohammad Haider <mohammadhaider.hamani@students.bfh.ch>
2023-09-16 22:26:34 +0200

    WIP plotting

--- frontend/src/components/plots/TaskTypeDistributionGraph.jsx
+++ frontend/src/components/plots/TaskTypeDistributionGraph.jsx
@@ -1,59 +1,89 @@
 import React, { useState, useEffect } from "react";
 import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
+  PieChart,
+  Pie,
   Tooltip,
   Legend,
+  Cell,
+  ResponsiveContainer
 } from "recharts";
+import {
+  Block,
+  BlockTitle,
+} from "framework7-react";
 import store from "../../js/store";
 
 const TaskTypeDistributionGraph = () => {
   const [statMatrix, setStatMatrix] = useState({});
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
   useEffect(() => {
-    setStatMatrix(store.getters.stats);
+    setStatMatrix(store.getters.stats.value);
   }, []);
 
-  const dataForGraph = Object.keys(statMatrix).map((year) => {
-    let yearData = { name: year };
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    };
 
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  const taskTypeCounts = {};
+
+  Object.keys(statMatrix).forEach((year) => {
     Object.keys(statMatrix[year]).forEach((month) => {
       Object.keys(statMatrix[year][month]).forEach((day) => {
         statMatrix[year][month][day].forEach((task) => {
-          if (!yearData[task.type]) yearData[task.type] = 0;
-          yearData[task.type]++;
+          if (!taskTypeCounts[task.taskType]) taskTypeCounts[task.taskType] = 0;
+          taskTypeCounts[task.taskType]++;
         });
       });
     });
-
-    return yearData;
   });
 
+  const dataForGraph = Object.keys(taskTypeCounts).map((taskType) => ({
+    name: taskType,
+    value: taskTypeCounts[taskType],
+  }));
+
   return (
-    <BarChart
-      width={500}
-      height={300}
-      data={dataForGraph}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      {/* Assuming task types are 'TaskType1', 'TaskType2', add or remove as necessary */}
-      <Bar dataKey="TaskType1" stackId="a" fill="#8884d8" />
-      <Bar dataKey="TaskType2" stackId="a" fill="#82ca9d" />
-      {/* ... add more Bar components as per your task types */}
-    </BarChart>
+    <Block>
+      <ResponsiveContainer width={windowSize.width * 0.95} height={windowSize.height * 0.5}>
+        <PieChart>
+          <Pie
+            data={dataForGraph}
+            cx={400}
+            cy={250}
+            labelLine={false}
+            label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+            outerRadius={200}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {dataForGraph.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={getColorByIndex(index)} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </Block>
   );
+};
+
+const getColorByIndex = (index) => {
+  const colors = ["#8884d8", "#82ca9d", "#ffc658", "#a4de6c", "#d0ed57"];
+  return colors[index % colors.length];
 };
 
 export default TaskTypeDistributionGraph;
--- frontend/src/components/plots/TimeSeriesGraph.jsx
+++ frontend/src/components/plots/TimeSeriesGraph.jsx
@@ -7,54 +7,82 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer
 } from "recharts";
+import {
+  Block,
+  BlockTitle,
+} from "framework7-react";
 import store from "../../js/store";
 
 const TimeSeriesGraph = () => {
   const [statMatrix, setStatMatrix] = useState({});
-  useEffect(() => {
-    setStatMatrix(store.getters.stats);
-  }, []);
-  const dataForGraph = Object.keys(statMatrix).map((year) => {
-    let yearData = { name: year };
-
-    Object.keys(statMatrix[year]).forEach((month) => {
-      yearData[month] = Object.keys(statMatrix[year][month]).reduce(
-        (acc, day) => acc + statMatrix[year][month][day].length,
-        0
-      );
-    });
-
-    return yearData;
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
   });
 
+  useEffect(() => {
+    setStatMatrix(store.getters.stats.value);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+  const dataForGraph = [];
+
+  Object.keys(statMatrix).forEach((year) => {
+    Object.keys(statMatrix[year]).forEach((month) => {
+      Object.keys(statMatrix[year][month]).forEach((day) => {
+        const dayRecord = statMatrix[year][month][day];
+        const taskCount = dayRecord.reduce((sum, record) => sum + 1, 0);
+        const dateString = `${day}/${month}/${year}`;
+        const existingDayData = dataForGraph.find((data) => data.name === dateString);
+
+        if (existingDayData) {
+          existingDayData.taskCount += taskCount;
+        } else {
+          dataForGraph.push({
+            name: dateString,
+            taskCount,
+          });
+        }
+      });
+    });
+  });
+
+
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={dataForGraph}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      {/* Assuming 'January', 'February', etc. as month keys */}
-      <Line
-        type="monotone"
-        dataKey="January"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-      <Line type="monotone" dataKey="February" stroke="#82ca9d" />
-      {/* ... add lines for other months */}
-    </LineChart>
+    <Block>
+      <ResponsiveContainer width={windowSize.width * 0.95} height={windowSize.height * 0.5}>
+        <LineChart
+          data={dataForGraph}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="taskCount" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </ResponsiveContainer>
+    </Block>
   );
 };
 
