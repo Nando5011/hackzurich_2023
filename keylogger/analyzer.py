f4330884507b559fd4d2b238f15857c8d6141751
Leo Hofer <leo.hofer@hofernet.ch>
2023-09-16 11:40:58 +0200

    analyzer class implemented

--- keylogger/analyzer.py
+++ keylogger/analyzer.py
@@ -1,15 +1,38 @@
 from keylogger import KeyLogger
 import time
 
-# Create a keylogger object
-klogger = KeyLogger(300)
+class Analyzer:
+    def __init__(self, maxLoggingTime: int):
+        self.keylogger = KeyLogger(maxLoggingTime)
+        self.MAX_LOGGING_TIME = maxLoggingTime
 
+    def getCPM(self):
+        shortTimeHistory = self.keylogger.getShortTimeHistory()
+        cpm = len(shortTimeHistory) / (self.MAX_LOGGING_TIME / 60)
+        return cpm
+    
+    def getWPM(self):
+        shortTimeHistory = self.keylogger.getShortTimeHistory()
+        wpm = len(shortTimeHistory) / (self.MAX_LOGGING_TIME / 60) / 5
+        return wpm
+    
+    def getSpeedVariation(self):
+        shortTimeHistory = self.keylogger.getShortTimeHistory()
+        speedVariation = 0
+        for i in range(len(shortTimeHistory) - 1):
+            timeDelta = shortTimeHistory[i + 1][1] - shortTimeHistory[i][1]
+            speedVariation += timeDelta.seconds
+        
+        if(len(shortTimeHistory) != 0):
+            speedVariation /= len(shortTimeHistory)
+            return speedVariation
+        else:
+            return 0
+
+analyzer = Analyzer(10)
+    
 while True:
-    # Get the short time history
-    shortTimeHistory = klogger.getShortTimeHistory()
-    #calcuilate cpm
-    cpm = len(shortTimeHistory) / 5
-    # Print cpm
-    print("CPM: " + str(cpm))
-    # Sleep 1 second
+    print("CPM: " + str(analyzer.getCPM()))
+    print("WPM: " + str(analyzer.getWPM()))
+    print("Speed Variation: " + str(analyzer.getSpeedVariation()))
     time.sleep(5)
