07b0f416bf92b315b91875407cec83b1a27aca0f
Florian Thiévent <florianjeremias@gmail.com>
2023-09-16 02:01:32 +0200

    feat: added hardware based serial function

---
+++ hardwareBasedSerial/hardwareSerialUUID.py
@@ -0,0 +1,31 @@
+import subprocess
+import sys
+
+def run(cmd):
+  try:
+    return subprocess.run(cmd, shell=True, capture_output=True, check=True, encoding="utf-8") \
+                     .stdout \
+                     .strip()
+  except:
+    return None
+
+def guid():
+  if sys.platform == 'darwin':
+    return run(
+      "ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'",
+    )
+
+  if sys.platform == 'win32' or sys.platform == 'cygwin' or sys.platform == 'msys':
+    return run('wmic csproduct get uuid').split('\n')[2] \
+                                         .strip()
+
+  if sys.platform.startswith('linux'):
+    return run('cat /var/lib/dbus/machine-id') or \
+           run('cat /etc/machine-id')
+
+  if sys.platform.startswith('openbsd') or sys.platform.startswith('freebsd'):
+    return run('cat /etc/hostid') or \
+           run('kenv -q smbios.system.uuid')
+  
+
+print(guid())
