72a8b915d4c0638005192ee0deee20cd80ffaaec
Florian Thiévent <florianjeremias@gmail.com>
2023-09-16 03:31:53 +0200

    feat: added systray init

---
+++ tray/startTray.py
@@ -0,0 +1,23 @@
+import pystray
+from PIL import Image, ImageDraw
+import os
+
+def create_image(width, height, color1, color2):
+    # Generate an image and draw a pattern
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+
+    return image
+
+
+# In order for the icon to be displayed, you must provide an icon
+icon = pystray.Icon('test name', icon=os.path.join(os.getcwd(), 'tray', 'logitech.png'))
+
+# To finally show you icon, call run
+icon.run()
