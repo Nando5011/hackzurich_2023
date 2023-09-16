885c3161725b725424e4f6781d8a50bd6ff0c25f
Florian Thiévent <florianjeremias@gmail.com>
2023-09-16 22:09:54 +0200

    feat: connection established to firebase

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from tray.firebaseclient import FirebaseClient
-from datetime import datetime
+from SWA.windowlogger import WindowLogger
+import datetime
+import time
+import threading
 
 
 
 if __name__ == '__main__':
-    
-    unix_timestamp = (datetime.now() - datetime(1970, 1, 1)).total_seconds() # Just for testing puirposes     
-    
-    
-    fb = FirebaseClient()
-    fb.test_connection(unix_timestamp, str(unix_timestamp)+"@check.mate")
-    
-    
-    
 
+    fb = FirebaseClient()
+    
+    windowLogger = WindowLogger(datetime.timedelta(seconds=1))
+    thread = threading.Thread(target=windowLogger.start)
+    thread.start()
+    print("Started window logger")
+    while True:
+        # pretty print the time per window
+        timePerWindow = windowLogger.getTimePerWindow()
+        print("Time per window:")
+        for key in timePerWindow:
+            fb.send_windowlogger_data_to_firestore(key, str(timePerWindow[key]))
+            
+        time.sleep(60)
