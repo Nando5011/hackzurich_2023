2d6552a39d0290ee871f6a94240bedfa5c0956b9
Florian Thiévent <florianjeremias@gmail.com>
2023-09-17 04:17:19 +0200

    chore: cleanup tray

--- tray/firebaseclient.py
+++ tray/firebaseclient.py
@@ -43,19 +43,19 @@ class FirebaseClient:
         
         application = key.split(os.path.sep)
         
-        date_data = {
-            "dummy": "breakwise date field"    
-        }
-        
+        mail_data = { "foo": "bar" }
+        date_data = { "dummy": "breakwise date field" }
         data = {
             "task": key,
             "taskType": "productive",
             "timeOpen": time_spent.isoformat()
         }
 
+        mail_path = "users/test1234@gmail.com"
         date_path = "users/test1234@gmail.com/devices/deviceID1/records/"+date.today().strftime('%Y-%m-%d')
         document_path = "users/test1234@gmail.com/devices/deviceID1/records/"+date.today().strftime('%Y-%m-%d')+"/timestamps/"+ time_spent.strftime('%H:%M:%S')
         
+        self.db.document(mail_path).set(mail_data, merge=True)
         self.db.document(date_path).set(date_data, merge=True)
         self.db.document(document_path).set(data, merge=True)
         
